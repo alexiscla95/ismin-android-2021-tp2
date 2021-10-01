7725cbfe3bfeec037888d747831a56be24492220
alexiscla95 <alexis-claesen@outlook.com>
2021-10-01 16:19:16 +0200

    first android interface

--- app/src/main/java/com/ismin/android/MainActivity.kt
+++ app/src/main/java/com/ismin/android/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.ismin.android
+import android.app.Activity
+import android.content.Intent
+import android.view.View
+import android.widget.EditText
+
 
 import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.ismin.androidtp1.Book
+import com.ismin.androidtp1.Bookshelf
 
 class MainActivity : AppCompatActivity() {
+    private var bookshelf: Bookshelf = Bookshelf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result: ActivityResult ->
+        if (result.resultCode == RESULT_OK) {
+            val someData = result.data?.getSerializableExtra(EXTRA_KEY)
+            bookshelf.addBook(someData as Book)
+
+            findViewById<TextView>(R.id.authorText).text = someData.author
+            findViewById<TextView>(R.id.titleText).text = someData.title
+            findViewById<TextView>(R.id.dateText).text = someData.date
+
+        }
+    }
+
+    fun goToCreation(view: View) {
+        val intent = Intent(this, CreateBookActivity::class.java)
+        startForResult.launch(intent)
+    }
+
+
+    fun addBook() {
+
     }
 }
